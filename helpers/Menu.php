fd919778a94908e797fbc9dbc3c241d26c19417f
Noppharat Dev <noppharat.dev@gmail.com>
2021-02-24 09:48:39 +0700

    first commit

---
+++ helpers/Menu.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Menu Items
+ * All Project Menu
+ * @category  Menu List
+ */
+
+class Menu{
+	
+	
+	public static $navbartopleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'post', 
+			'label' => 'Post', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'users', 
+			'label' => 'Users', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'categorys', 
+			'label' => 'Categorys', 
+			'icon' => ''
+		)
+	);
+
+	public static $navbarsideleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'post', 
+			'label' => 'Post', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'users', 
+			'label' => 'Users', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'categorys', 
+			'label' => 'Categorys', 
+			'icon' => ''
+		)
+	);
+
+	
+	
+	public static $headline = array();
+
+}
